d676651c77d9f59f04e21f3fa6bb8eb05a594842
AAlon <avishaya@amazon.com>
2019-07-31 16:29:37 -0700

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt-get update && apt-get install --no-install-recommends -y wget jq python3-rosinstall python3-colcon-common-extensions ros-dashing-ros2bag ros-dashing-rosbag2
+apt-get update && apt-get install --no-install-recommends -y wget jq python3-rosinstall python3-colcon-common-extensions ros-dashing-ros2bag ros-dashing-rosbag2 ros-dashing-rosbag2-storage-default-plugins
 pip3 install awscli --upgrade
 
