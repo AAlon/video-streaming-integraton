bb88b7e9b3436d63025112a1ce036f5e4b4dd187
AAlon <avishaya@amazon.com>
2019-07-30 21:45:00 -0700

    Update prepare_bag.sh

--- prepare_bag.sh
+++ prepare_bag.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-wget https://aws-robomaker-sample-resources.s3-us-west-2.amazonaws.com/rosbags/jb_presentation_stream.tar.gz
+extract_to=$1
+curr_dir=${pwd}
+
+cd ${extract_to}
+
+wget https://aws-robomaker-sample-resources.s3-us-west-2.amazonaws.com/rosbags/jb_presentation_stream.tar.gz 
 tar -xvzf jb_presentation_stream.tar.gz
 rm jb_presentation_stream.tar.gz
+
+cd ${curr_dir}
